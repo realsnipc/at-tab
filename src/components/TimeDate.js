1a7a59ff21c7dce98bb63d9df07ebfc93e4e62ef
snipc <snipc.mail@proton.me>
2023-12-16 22:39:09 +0530

    Add 12 hour clock system

--- src/components/TimeDate.js
+++ src/components/TimeDate.js
@@ -4,19 +4,34 @@ import './styles/TimeDate.scss'
 
 export default () => {
   const [now, setNow] = useState(new Date())
+  const [date, setDate] = useState()
+
+
 
   useEffect(() => {
     setInterval(() => setNow(new Date()), 1000)
   }, [])
 
+  useEffect(() => {
+
+    function return12Hours() {
+      let hour=now.toLocaleTimeString(undefined, {
+        hour: "numeric"
+      })
+      if(hour>12){
+        hour= hour-12
+        console.log(hour)
+      }
+         setDate(hour + ':' + now.getMinutes())
+    }
+    return12Hours()
+  }, [])
+  
+
   return (
     <>
       <h1 className="time">
-        {now.toLocaleTimeString(undefined, {
-          hour: "numeric",
-          minute: "numeric",
-          second: undefined,
-        })}
+        {date}
       </h1>
       <h2 className="date">
         {now.toLocaleDateString(undefined, {
